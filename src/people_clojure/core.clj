20f70e2870ae8e86c13569c62c76da8117a6a2cb
Matthew Scofield <matthewgscofield@outlook.com>
2017-01-11 13:35:27 -0600

    added web functionality and sorting

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,9 +1,12 @@
 (ns people-clojure.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 ;;comment
-(defn -main []
-  (println "Type a country name:")
+
+(defn read-people []
  (let [people (slurp "people.csv") 
        people (str/split-lines people)
        people (map (fn [line]
@@ -13,12 +16,32 @@
        people (rest people)
        people (map (fn [line]
                      (zipmap header line))
-                people)
-       country-name (read-line)
-       people (filter (fn [line]
-                        (= (get line "country") country-name))
-                people)
-       file-text (pr-str people)]
-   (spit "filtered_people.edn" file-text)))
+                people)]
+   people))
+
+(defn people-html [country]
+  (let [people (read-people)
+        people (if (= 0 (count country))
+                 people
+                      (filter (fn [person]
+                                (= (get person "country") country))
+                        people))]
+    [:ol
+     (map (fn [person]
+            [:li (str (get person "first_name") " " (get person "last_name"))])
+       people)]))
+  
+
+(c/defroutes app
+  (c/GET "/:country{.*}" [country]
+    (h/html [:html
+             [:body
+              (people-html country)]])))
+
+(defonce server (atom nil))
+(defn -main []
+  (when @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
    
  
